59c4196e2ca6de91e7cc72def252dd7f9c58cfd2
pascaie <enrico.pascai@gmail.com>
2018-11-25 04:20:26 +0100

    small modifications to the look

--- test_scripts/helloworld.py
+++ test_scripts/helloworld.py
@@ -1,14 +1,14 @@
-#first Python program on my Ubuntu virtual machine
-#date: November 19th, 2018
+# first Python program on my Ubuntu virtual machine
+# date: November 19th, 2018
 print("-----")
 print("hello world!")
 print("-----")
 
-#variable assignment and print
+# variable assignment and print
 date = "November 15th, 2018"
 print("today is " + date)
 
-#mi to km conversion
+# mi to km conversion
 miles = 500
 kilometers = miles * 1.60934
 print(str(miles) + " miles are equal to " + str(kilometers) + " kilometers")
--- test_scripts/modules.py
+++ test_scripts/modules.py
@@ -1,13 +1,13 @@
-#modules
+# modules
 
-#import datetime module and use its date() function
+# import datetime module and use its date() function
 import datetime
 birthday = datetime.date(1992, 7, 17)
 print("I was born on " + str(birthday))
 
-#import specific functions
+# import specific functions
 from datetime import date, time as t
-sisterBirthday = date(1997,6,11) #can invoke the date() function without specifying the parent module
+sisterBirthday = date(1997,6,11) # can invoke the date() function without specifying the parent module
 print("My sister was born on " + str(sisterBirthday))
 date.today()
 print("Today is " + str(date.today()))
@@ -16,12 +16,11 @@ setTime = t(19, 35, 50)
 print(setTime)
 print(setTime.hour)
 
-import sys #module of system-specific parameters and functions
-print(sys.path) #displays the paths which are followed by the Python engine when searching for packages/modules
+import sys # module of system-specific parameters and functions
+print(sys.path) # displays the paths which are followed by the Python engine when searching for packages/modules
 
 import actionPackage.action
 actionPackage.action.printSomething()
 
 import printModule
 printModule.printWord("ciao")
-
