cfa884d3cb08f26dd57ac928b6fca1efac739fc7
pascaie <enrico.pascai@gmail.com>
2018-11-26 02:28:38 +0100

    finalized script to calculate the amount due. control statements missing

--- test_app/conti_bollette.py
+++ test_app/conti_bollette.py
@@ -1,41 +1,49 @@
 # inputs:
 # total cost of the bill
 # month of the year
+# year
 # number of Rosario's days abroad
+
+# singleDayCost_perCapita = tot amount / (giorni del mese * 2)
+# RosDayInHouse = giorni mese - giorni fuori
+# RosAmount = singleDayCost_perCapita * RosDayInHouse
+# LandLordAmount = BillTOT - RosAmount
 
 import sys
 import calendar
 from datetime import date as dt
 
 supportEmail = 'enrico.pascai@gmail.com'
-thisMonth = dt.today().month
 
 class Bill:
+    def __init__(self):
+        self.amount = None
+        self.month = None
+        self.year = None
+        self.daysAbroad = None
+    def calculate_amountPerCapita(self, totAmount, month, year, daysAbroad):
+        amount_perCapita = [0, 0]
+        daysInMonth = calendar.monthrange(year, month)[1]
+        singleDayCost_perCapita = totAmount / (daysInMonth * 2)
+        RosDaysInHouse = daysInMonth - daysAbroad
+        amount_perCapita[0] = singleDayCost_perCapita * RosDaysInHouse #RosAmount
+        amount_perCapita[1] = totAmount - amount_perCapita[0] #LandLordAmount
+        return amount_perCapita
 
+bill = Bill()
+bill.amount = float(input('Please, enter the total amount of the bill: '))
+bill.month = int(input('Please, enter the month of the bill '
+                    '(1 = January; 12 = December): '))
+bill.year = int(input('Please, enter the year of the bill: '))
+bill.daysAbroad = int(input('Please, enter your number of days abroad: '))
 
-billAmount = input('Please, enter the total amount of the bill: ')
-billMonth = input('Please, enter the month of the bill '
-                    '(1 = January; 12 = December)')
+print('------------------------------------------------------\nRECAP')
+print('Amount of the bill: ' + str(bill.amount) + '€')
+print('Month&Year of the bill: ' + calendar.month_name[bill.month] + ' ' + str(bill.year))
+print('Number of days abroad: ' + str(bill.daysAbroad) + ' / ' + str(calendar.monthrange(bill.year, bill.month)[1]))
 
+amount_perCapita = bill.calculate_amountPerCapita(bill.amount, bill.month, bill.year, bill.daysAbroad)
 
-selectMonth = input('Is the bill relative to the current month ('
-                + calendar.month_name[thisMonth]
-                + ')? [y/n] ')
-
-if selectMonth in ('n', 'N', 'no', 'NO'):
-    selectMonth = input('Is the bill relative to the previous month ('
-                    + calendar.month_name[thisMonth - 1]
-                    + ')? [y/n] ')
-    if selectMonth in ('n', 'N', 'no', 'NO'):
-        print('Sorry, this script was not thought to take into account bills dated earlier than two monts from your current date')
-        print('For this purpose, we may have to set up a cluster to handle the increased computational effort, with the related additional costs for the end user')
-        print('Please, contact the support team by email (' + supportEmail + ') or wait for release 2.0')
-        sys.exit()
-    else:
-        monthVar = thisMonth - 1
-else:
-    monthVar = thisMonth
-
-print(monthVar)
-
-print(calendar.monthrange(2018, 11)[1])
+print('------------------------------------------------------\nAMOUNT DUE')
+print('Your amount (Rosario) is equal to ' + str(round(amount_perCapita[0], 1)) + '€')
+print('Landlord\'s amount is equal to ' + str(round(amount_perCapita[1], 1)) + '€')
